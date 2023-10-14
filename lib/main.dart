6248eb9cb87e29403f569ef8fdb4a305f7fa0f8f
https://github.com/Sharminsultina <sultinasharmin101@gmail.com>
2023-10-14 14:13:31 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-;
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ImageFeed(),
+    );
+  }
+}
+
+class ImageFeed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+    final crossAxisCount = orientation == Orientation.portrait ? 1 : 2;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Feed'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: crossAxisCount,
+        ),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+            padding: EdgeInsets.all(8.0),
+            child: Card(
+              elevation: 4.0,
+              child: Image.network(
+                'https://via.placeholder.com/150',
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
